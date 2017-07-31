fd0e825687d39ab522d39f0eb759b3bc7403b027
Pieter van Laatum <Pieter.Laatum@student.ru.nl>
2017-07-31 13:01:32 +0200

    test

--- src/app.js
+++ src/app.js
@@ -45,9 +45,9 @@ function processEvent(event) {
                 let resolvedQuery = response.result.resolvedQuery
                 let action = response.result.action; //actie in intent
                 let intent = response.result.metadata.intentId;
+                    console.log(responseData);
 
                 if (isDefined(responseData) && isDefined(responseData.facebook)) {
-                    console.log(responseData);
                     if (!Array.isArray(responseData.facebook)) {
                         try {
                             console.log('Response as formatted message');
