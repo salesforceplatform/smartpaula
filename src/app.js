150f82774b507223d06a4bbfac99124bcb87d3cf
Pieter van Laatum <Pieter.Laatum@student.ru.nl>
2017-07-31 12:03:02 +0200

    test

--- src/app.js
+++ src/app.js
@@ -46,6 +46,8 @@ function processEvent(event) {
                 let action = response.result.action; //actie in intent
                 let intent = response.result.metadata.intentId;
 
+                console.log(sender);
+
                 if (isDefined(responseData) && isDefined(responseData.facebook)) {
                     if (!Array.isArray(responseData.facebook)) {
                         try {
