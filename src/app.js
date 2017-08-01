82f106d923f5c1a1e9a17d97db492b442b741c60
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-01 16:06:02 +0200

    vragenlijst tracking

--- src/app.js
+++ src/app.js
@@ -130,16 +130,15 @@ function handleResponse(response, sender) {
                                     });
                                 });
 
-                            if (isDefined(payload)) {
-                                if (isDefined(payload.vragenlijst_end) && payload.vragenlijst_end) {
+                            if (isDefined(payload) && isDefined(payload.vragenlijst_end) && payload.vragenlijst_end) {
+
                                     client.query('SELECT id FROM vragenlijsten WHERE fbuser = $1 ORDER BY gestart DESC LIMIT 1', [sender]).then(res => {
                                         let vragenlijst = res.rows[0].id;
                                         client.query('UPDATE vragenlijsten set gestopt = (SELECT NOW()) WHERE id = $1', [vragenlijst]);
                                     });
-                                } else {
-                                    message.quick_replies = quickReplies;
-                                }
-                            } 
+                            } else {
+                                message.quickReplies = quickReplies;
+                            }
                         });
                     }
                     
