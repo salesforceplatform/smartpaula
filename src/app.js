3326620782dc620409f1d25c28688ad95d86ebe1
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-02 14:36:32 +0200

    Nokia connect

--- src/app.js
+++ src/app.js
@@ -380,7 +380,7 @@ function subscribeToNokia(fbuser) {
             let url = 'https://api.health.nokia.com/notify';
             console.log('subscribing: ', row, url);
 
-            nokiaAPI.get('http://api.health.nokia.com/measure?action=getmeas&userid=' + row.nokia_user, row.oauth_access_token, row.oauth_access_secret,
+            nokiaAPI.get('https://api.health.nokia.com/measure?action=getmeas&userid=' + row.nokia_user, row.oauth_access_token, row.oauth_access_secret,
                 (error, responseData, result) => {
 
                     if (error) {
