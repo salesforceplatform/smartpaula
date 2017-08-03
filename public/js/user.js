f39ce0f13bc9c9c65235351d3b434c5b36224538
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-04 00:27:56 +0200

    Portal

--- public/js/user.js
+++ public/js/user.js
@@ -1,6 +1,6 @@
 $(function () {
     $.get(window.location.href + '/data', function (data) {
-        var ctx = $('#questionnare_overall').getContext('2d');        
+        var ctx = $('#questionnare_overall')[0].getContext('2d');        
         pam_chart = new Chart(ctx,
             {
                 type: 'line',
