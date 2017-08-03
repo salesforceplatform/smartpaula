3325ad0d9dce570572f32420dd6a7c39cd11ea7d
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-03 23:05:58 +0200

    Portal

--- src/portal.js
+++ src/portal.js
@@ -6,24 +6,44 @@ const { Pool, Client } = require('pg');
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 router.get('/', function (req, res) {
-    pool.query("SELECT fbuser FROM vragenlijsten GROUP BY fbuser UNION SELECT fbuser FROM connect_nokia").then(result => {
-        let users = [];
-        result.rows.forEach(row => {
-            users.push(row.fbuser);
-        })
-        res.render('index', { title: 'Hey', message: 'Hello there!', users: users });
-    });
+    try {
+        pool.query("SELECT fbuser FROM vragenlijsten GROUP BY fbuser UNION SELECT fbuser FROM connect_nokia").then(result => {
+            let users = [];
+            result.rows.forEach(row => {
+                users.push(row.fbuser);
+            })
+            res.render('index', { title: 'Hey', message: 'Hello there!', users: users });
+        });
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
+    }
 });
 
 router.post('/', (req, res) => {
-    let user = req.body.user;
-    res.redirect('/portal' + user);
+    try {
+        let user = req.body.user;
+        res.redirect('/portal' + user);
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
+    }
 });
 
 router.get('/:user', (res, req) => {
-    let user = req.params.fbuser                        
-    res.render('user', { user: user });
-
+    try {
+        let user = req.params.fbuser
+        res.render('user', { user: user });
+    } catch (err) {
+        return res.status(400).json({
+            status: "error",
+            error: err
+        });
+    }
 })
 
 module.exports = router;
