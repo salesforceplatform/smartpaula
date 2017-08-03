219495bb52660d1dc855afc4284837d6bff9483e
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-03 18:18:13 +0200

    portal

--- src/portal.js
+++ src/portal.js
@@ -1,8 +1,8 @@
 ﻿const express = require('express')
-const portal = express.Router()
+const router = express.Router()
 
-module.exports = function (portal) {
-    portal.get('/', function (req, res) {
-        res.send('portal!');
-    })
-}
+router.get('/', function (req, res) {
+    res.send('portal!');
+})
+
+module.exports = router;
