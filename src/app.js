b2642ca0516ca42e44422709eb8e45ca2ee97d46
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-03 14:49:58 +0200

    Nokia data

--- src/app.js
+++ src/app.js
@@ -372,7 +372,7 @@ function getNokiaMeasurements(userid, callback) {
     pool.query('SELECT *, extract(epoch from last_update) as time FROM connect_nokia WHERE fbuser = $1 OR nokia_user = $1', [userid]).then(res => {
         let user = res.rows[0];
         if (isDefined(user)) {
-            let url = 'https://api.health.nokia.com/measure' + '?action=getmeas' + '&userid=' + user.nokia_user + '&lastupdate=' + user.time;
+            let url = 'https://api.health.nokia.com/measure' + '?action=getmeas' + '&userid=' + user.nokia_user + '&lastupdate=' + Math.round(user.time);
             let signedUrl = nokiaAPI.signUrl(url, user.oauth_access_token, user.oauth_access_secret);
             console.log(signedUrl);
             console.log(user);
