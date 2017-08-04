030f21f7c46e8ddcff6cb88d8ab3c9c6d1171e55
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-04 16:40:48 +0200

    Portal

--- public/js/user.js
+++ public/js/user.js
@@ -31,6 +31,36 @@ $(function () {
                         }]
                     }
                 }
+            });
+
+        ctx = $('#questionnare_per_question')[0].getContext(';2d')
+        question_chart = new Chart(ctx,
+            {
+                type: 'line',
+                data: {
+                    datasets: data.questions.data
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 52
+                            }
+                        }],
+                        xAxes: [{
+                            type: 'time',
+                            time: {
+                                unit: 'day',
+                                unitStepSize: 1,
+                                tooltipFormat: "h:mm:ss a",
+                                displayFormats: {
+                                    hour: 'MMM D, h:mm A'
+                                }
+                            }
+                        }]
+                    }
+                }
             })
     });
 });
