0d9bf495ca2dd69e04ace89489907c4b2a81b172
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-07 12:31:01 +0200

    Portal

--- src/portal.js
+++ src/portal.js
@@ -103,9 +103,9 @@ router.get('/:user/data', (req, res) => {
             userData.questions.data = Object.keys(userData.questions.data).map(function (key) { return userData.questions.data[key] })
 
             pool.query('SELECT *, to_char(timezone(\'zulu\', to_timestamp(date_part(\'epoch\', measure_blood.measure_date))),\'YYYY-MM-DDThh24:MI:SSZ\') as date FROM measure_blood WHERE fbuser = $1', [user]).then((result) => {
-                userData.systolic = [];
-                userdata.diastolic = [];
-                userData.pulse = [];
+                userData.blood.systolic = [];
+                userdata.blood.diastolic = [];
+                userData.blood.pulse = [];
                 result.rows.forEach(row => {
                     if (row.systolic) {
                         userData.systolic.push({ x: row.date, y: row.systolic });
