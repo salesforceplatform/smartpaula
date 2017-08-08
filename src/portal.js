23b89cf71b707f2da75c3c81a2583c4690605b0f
Pieter van Laatum <Pieter901@hotmail.com>
2017-08-08 15:06:38 +0200

    Login

--- src/portal.js
+++ src/portal.js
@@ -168,7 +168,7 @@ passport.use('local-login', new LocalStrategy({
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'email': email }, function (err, user) {
+        User.findOne({ where: { 'email': email } }, function (err, user) {
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
